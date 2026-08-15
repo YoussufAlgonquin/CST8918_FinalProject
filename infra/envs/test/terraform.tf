6fe2d5e2cbc359956b0589ff4551ef524a4ea101
Faiza2201 <boud0219@algonquinlive.com>
2026-08-14 20:07:19 -0400

    Upgrade Terraform version and provider constraints

--- infra/envs/test/terraform.tf
+++ infra/envs/test/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.7"
+  required_version = ">= 1.9"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
